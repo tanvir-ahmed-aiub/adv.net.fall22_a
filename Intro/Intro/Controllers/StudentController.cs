539c975a4d4ddc12b2e1ee831e82546937243399
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2022-09-21 13:36:59 +0600

    Intro to .net

---
+++ Intro/Intro/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intro.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.Section = "B";
+            ViewData["Course"] = "Adv .Net";
+            return View();
+        }
+        public ActionResult Projects() {
+            //return Redirect("https://www.aiub.edu");
+            return View("Index");
+            //return RedirectToAction("About", "Home");
+            //return RedirectToAction("Index");
+        }
+        
+    }
+}
