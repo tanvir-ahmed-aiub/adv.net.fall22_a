c9653b5430493d14ae4c0b974c6f08446fc6d608
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2022-09-26 20:47:26 +0600

    week 2 lectures

---
+++ Intro/Intro/Controllers/BookController.cs
@@ -0,0 +1,60 @@
+﻿using Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Intro.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            //TempData["msg"] = "Your task was successful";
+            //String[] books = new string[] {"Himu","The alchemist","Misir Ali","Byomkesh","Harry Potter" };
+            //ViewBag.books = books;
+            //return View();
+
+            List<Book> books = new List<Book>();
+            for(int i = 1; i <= 10; i++)
+            {
+                var book = new Book() {
+                    Id = i,
+                    Title = "Book " + i,
+                    Author = "Author of Book " + i,
+                    Edition = 1
+                };
+                books.Add(book);
+            }
+            return View(books);
+            //return RedirectToAction("Index","Home");
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book book) {
+            //ViewBag.BookName = Request["Title"];
+            //
+            //ViewBag.BookName = form["Title"];
+            ViewBag.BookName = book.Author;
+            return View();   
+        }
+
+        public ActionResult Details(int id) {
+            Book book = new Book() { 
+                Id = id,
+                Title = "Book " + id,
+                Author = "Author of Book " + id,
+                Edition = 1,
+            };
+                
+            return View(book);
+        }
+    }
+}
---
+++ Intro/Intro/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Intro.Models
+{
+    public class Book
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Edition { get; set; }
+
+    }
+}
