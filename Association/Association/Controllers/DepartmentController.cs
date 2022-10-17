1a397a7d99ea545cdf275fd9ea5c16a712fcaa26
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2022-10-17 14:03:03 +0600

    Association

---
+++ Association/Association/Controllers/DepartmentController.cs
@@ -0,0 +1,37 @@
+﻿using Association.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Association.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        public ActionResult Index()
+        {
+            UMS_fall22_aEntities db = new UMS_fall22_aEntities();
+
+            return View(db.Departments.ToList());
+        }
+        public ActionResult Details(int id)
+        {
+            UMS_fall22_aEntities db = new UMS_fall22_aEntities();
+            var d = (from dept in db.Departments
+                     where dept.Id == id
+                     select dept).SingleOrDefault();
+            //var students = (from student in db.Students
+            //                where student.DepartmentId == id
+            //                select student).ToList();
+            //ViewBag.Students = students;
+            //var courses = (from c in db.Courses
+            //               where c.DepartmentId == id
+            //               select c).ToList();
+            //ViewBag.Courses = courses;
+            return View(d);
+        }
+
+    }
+}
---
+++ Association/Association/Controllers/RegistrationController.cs
@@ -0,0 +1,35 @@
+﻿using Association.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Association.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        [HttpGet]
+        public ActionResult Index()
+        {
+            UMS_fall22_aEntities db = new UMS_fall22_aEntities();
+            return View(db.Courses.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(int[] courses)
+        {
+            UMS_fall22_aEntities db = new UMS_fall22_aEntities();
+            foreach (var course in courses) {
+                db.CourseStudents.Add(new CourseStudent()
+                {
+                    CourseId = course,
+                    StudentId = 2
+                });
+
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
